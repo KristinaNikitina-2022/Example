d6da0cff235f2bcf164baac550e625388c977695
Kristina <mirabell2012@mail.ru>
2022-10-16 21:39:47 +0300

    add Example005_ifelse

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя");
+string username = Console.ReadLine();
+
+if (username == "Маша");
+{
+    Console.WriteLine("Ура,это же Маша");
+}
+
+{
+       Console.Write("Привет,");
+       Console.WriteLine(username);
+}    
